8f137d3345bb6d1180053de3c939449875007b11
elliott <elliott@wlan802-148.resnet.cwu.edu>
2017-01-11 13:26:56 -0800

    Adding services directory for backend python code

---
+++ website/index.js
@@ -0,0 +1,9 @@
+function show_admin_login(){
+  document.getElementById("textStuff").innerHTML="Testing";
+}
+function show_training(){
+
+}
+function show_about(){
+
+}
--- website/svg_builder.js
+++ website/svg_builder.js
@@ -57,4 +57,8 @@ $(document).ready(function() {
     build();
     build();
     build();
+    build();
+    build();
+    build();
+    build();
 });
