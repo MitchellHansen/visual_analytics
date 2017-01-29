8e0de1b8266654eb633ebf64a8e9ffca374e5211
MitchellHansen <mitchellhansen4@gmail.com>
2017-01-28 19:11:40 -0800

    Working more on the site, admin page is going somewhat

--- static-website/index.js
+++ static-website/index.js
@@ -1,189 +1,113 @@
 
+var logged_in = false;
+var token = "asdofijwer";
+var selected_trial = "";
+var selected_template = "";
 
-logged_in = false;
-token = "asdofijwer";
-
-function show_admin_login(){
-    $("#right-hand-container").children().hide();
-    $("#admin-login-div").show();
+function admin_login_handler(){
+    var admin_credentials_combo = $("#admin-login-form").serializeArray();
+    admin_login(admin_credentials_combo[0].value, admin_credentials_combo[1].value);
 }
-
-//  ---------------- API CALL TO { /auth/admin_login }
-
-function admin_login(){
-
-//    $.ajax({
-//
-//        url: "/auth/admin_login",
-//        contentType: "application/json; charset=utf-8",
-//        type: "POST",
-//        async: false,
-//
-//        data :{
-//            email : $('emailInput').val(),
-//            password : $('passwordInput').val()
-//        },
-//
-//        // We are going to need a way to verify our logged in status, while
-//        // disallowing users the access to a "logged_in=true" variable.
-//        // We could return a "one time code" when one logs in
-//        success: function(result){
-//
-//            // auth_token = result.key;
-//            // From that we could then interact with the api using this token
-//
-//            console.log(result);
-//
-//            if (result == "success"){
-//                logged_in = true;
-//                show_admin_window();
-//            }
-//            else{
-//                // Show help message
-//            }
-//        }
-//
-//        // The only difficulty here is that we will need to cache these codes for a time period.
-//        // Perhaps we can have another table which contains all the generated codes and their expiration
-//        // stamp along with the admin ID for which they were generated for.
-//
-//    });
-
-    // Lets just pretend that returns success
-    var result = "success";
-    if (result == "success"){
-        logged_in = true;
-        show_admin_window();
-    }
-    else{
-        // Show help message
-    }
-
+function trial_login_handler(){
+    var login_code = $("#trial-login-form").serializeArray();
+    var trial_data = trial_login(login_code[0].value);
 }
-
-
-function show_training(){
-    $.ajax({url: "http://mitchellhansen.info/", async: false, success: function(result){
-        console.log(result);
-    }});
-
-    // we need an animation to hide this title screen and pull up the trial screen
+function begin_training_handler(){
 }
-function show_about(){
-    $("#right-hand-container").children().hide();
-    $("#about-div").show();
-}
-
-function show_trial_login(){
-    $("#right-hand-container").children().hide();
-    $("#trial-login-div").show();
-}
-
-function test(){
-    roll_up_reveal_navbar();
-}
-
-function roll_up_reveal_navbar(){
-    $("#home-row").slideUp();
-    $("#navbar-row").show();
-    $("#slider-container").show();
-}
-
-function show_admin_window(){
-    roll_up_reveal_navbar();
-    get_test_statuses();
-}
-
-//  ---------------- API CALL TO { /auth/get_test_statuses }
-function get_test_statuses(){
-
-    if (!logged_in || token == ""){
-        // Not logged in
-    }
-
-//    $.ajax({
-//
-//        url: "/auth/get_bulk_test_data",
-//        contentType: "application/json; charset=utf-8",
-//        type: "POST",
-//        async: false,
-//
-//        data :{
-//            token  : token,
-//            status : { Either running, complete, parked, or any } perhaps as strings?
-//        },
-//
-//        success: function(result){
-//
-//            console.log(result);
-//
-//        }
-//    });
-
-    var dummy_data = {"test1":"RUNNING", "test2":"RUNNING", "test3":"COMPLETE", "test4":"PARKED", "test5":"COMPLETE"};
-
-    // Lets just pretend that returns success
-    var result = "success";
-
-    if (result == "success"){
-        populate_admin_page_active_tests(dummy_data);
-        //populate_admin_page_active_tests(result.test_statuses);
-    }
-    else{
-        // Show help message
-    }
+function begin_trial(trial_data){
 }
 
 function populate_admin_page_active_tests(test_statuses){
 
+    // Remove all the old data
+    $("#test-status-list").empty();
+
+    // For each of the tests the we received back
 
     for (var key in test_statuses) {
         if (test_statuses.hasOwnProperty(key)) {
 
-            var elem = $("#test-status-list-element-template").clone();
-            $(elem).attr("id", key);
+            var id = "test-" + key;
 
+            // Clone the template element
+            var elem = $("#test-status-list-element-template").clone();
+            $(elem).attr("id", id);
+
+            // Get it's status
             if (test_statuses[key] == "RUNNING"){
 
+                // Set the right hand symbol text & the left hand test name text
                 $(elem).children().children().last().text("⏲");
-                $(elem).children().children().first().text(key);
-
             }
             else  if (test_statuses[key] == "PARKED"){
 
                 $(elem).children().children().last().text("✔");
-                $(elem).children().children().first().text(key);
-
             }
             else  if (test_statuses[key] == "COMPLETE"){
 
                 $(elem).children().children().last().text("🛑");
-                $(elem).children().children().first().text(key);
-
             }
             else {
                 $(elem).children().children().last().text("ERROR");
-                $(elem).children().children().first().text(key);
             }
 
+            // Set the button text
+            $(elem).children().children().first().text(key);
 
+            // Set the onclick for the button
+             $(elem).children().children().first().attr("onclick", "set_trial_selection(\"" + id + "\")");
+
+            // Append our cloned element to the scroll box
             $("#test-status-list").append(elem);
-            $("#" + key).show();
+
+            // Make sure that we un-hid it
+            $("#" + id).show();
         }
     }
 }
 
+function populate_admin_page_test_templates(test_templates){
 
+    for (var key in test_templates) {
+        if (test_templates.hasOwnProperty(key)) {
 
+            var id = "template-" + key;
 
+            // clone the element, set it's id to be the name of the template
+            var elem = $("#test-status-list-element-template").clone();
+            $(elem).attr("id", id);
 
+            // Set the onclick to select that name when clicked
+            // I hate web
+            $(elem).children().children().first().attr("onclick", "set_template_selection(\"" + id + "\")");
 
+            // set the button text and graph type text
+            $(elem).children().children().last().text(test_templates[key]);
+            $(elem).children().children().first().text(key);
 
+            // Add it to the list and show it
+            $("#test-template-list").append(elem);
+            $("#" + id).show();
+        }
+    }
+}
 
+function set_template_selection(template_name){
+    if (selected_template != ""){
+        $("#" + selected_template).children().children().first().toggleClass("w3-green");
+    }
+    selected_template = template_name;
+    $("#" + selected_template).children().children().first().toggleClass("w3-green");
+}
 
+function set_trial_selection(trial_name){
+   if (selected_trial != ""){
+        $("#" + selected_trial).children().children().first().toggleClass("w3-green");
+    }
+    selected_trial = trial_name;
+    $("#" + selected_trial).children().children().first().toggleClass("w3-green");
 
-
-
+}
 
 
 
