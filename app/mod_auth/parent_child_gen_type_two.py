429cba11ec71cddceb052350c1136a81eb0aaf1e
Elliott Barcoe-Walsh <elliottwalsh94@gmail.com>
2017-03-09 16:03:24 -0800

    Update parent_child_gen_type_two.py

--- app/mod_auth/parent_child_gen_type_two.py
+++ app/mod_auth/parent_child_gen_type_two.py
@@ -1,6 +1,7 @@
-﻿#Created by Anthony Armatas for CS 481 Visual Analytics
+#Created by Anthony Armatas for CS 481 Visual Analytics
 #Imports the randint function from random. Could have also done import random,randint
 from random import randint
+import math
 import copy
 #The assumtion of this file is that the admin will give the datapoints, but the test
 #will only create 20 graphs
@@ -9,99 +10,44 @@ import copy
 class gen_two:
 
     @staticmethod
-    def genList(dLst,amountOfDP):
-        for i in range(amountOfDP):
-            dataPnt = randint(0, 100)
-            dLst[i] = dataPnt
-        return dLst
+    def whiteNoise (parent_data):
 
+        child_lists = []        
+        weight = 0.10
+        for x in range(9):
+            
+            child = []
+            for i in parent_data:
+		val = (i * weight) + (randint(0, 100) * (1-weight))
+		val = max(min(val, 100), 0) # clamp the value [0-100]
+                child.append(int(math.floor(val)))
+            weight += 0.10
+            child_lists.append(child)
 
-    @staticmethod
-    def whiteNoise (childList, amountOfDP, noisePercentFactor):
-
-        classVectorPercent = 1 - noisePercentFactor
-
-        for i in range(len(childList)):
-
-            child_noise_array = [None] * amountOfDP
-            for j in range(amountOfDP):
-                child_noise_array[j] = (randint(3, 10)) * noisePercentFactor
-                childList[i][j] = childList[i][j] * classVectorPercent + (randint(3, 10)) * noisePercentFactor
-
+        return child_lists
 
     @staticmethod
     def gen_Parents_And_Children_type_two(amountOfDP):
-        #Python Dictionary to be returned
-        the_return_dict = {}
+
+        parents_class1 = []
+        parents_class2 = []
+        children_class1 = []
+        chlidren_class2 = []
+
         amountOfDP = int(amountOfDP)
 
-        parentA = [None] * amountOfDP
-        parentB = [None] * amountOfDP
-        gen_two.genList(parentA,amountOfDP)
-        gen_two.genList(parentB,amountOfDP)
-        listOParents = []
-        #Added these lists to separate the children graphs from each other for the databases
-        listOClass1Childern = []
-        listOClass2Childern = []
-        listOChildren = [listOClass1Childern, listOClass2Childern]
-        containerOfPandC = [listOParents,listOChildren]
-        listOParents.append(parentA)
-        listOParents.append(parentB)
+        for i in range(amountOfDP):
+            parents_class1.append(randint(5, 95))
+            parents_class2.append(randint(5, 95))
 
-        child1 = copy.deepcopy(parentA)
-        child3 = copy.deepcopy(parentA)
-        child5 = copy.deepcopy(parentA)
-        child7 = copy.deepcopy(parentA)
-        child9 = copy.deepcopy(parentA)
-        child11 = copy.deepcopy(parentA)
-        child13 = copy.deepcopy(parentA)
-        child15 = copy.deepcopy(parentA)
-        child17 = copy.deepcopy(parentA)
+        children_class1 = gen_two.whiteNoise(parents_class1)
+        children_class2 = gen_two.whiteNoise(parents_class2)
 
-        child2 = copy.deepcopy(parentB)
-        child4 = copy.deepcopy(parentB)
-        child6 = copy.deepcopy(parentB)
-        child8 = copy.deepcopy(parentB)
-        child10 = copy.deepcopy(parentB)
-        child12 = copy.deepcopy(parentB)
-        child14 = copy.deepcopy(parentB)
-        child16 = copy.deepcopy(parentB)
-        child18 = copy.deepcopy(parentB)
-        listOClass1Childern.append(child1)
-        listOClass2Childern.append(child2)
-        listOClass1Childern.append(child3)
-        listOClass2Childern.append(child4)
-        listOClass1Childern.append(child5)
-        listOClass2Childern.append(child6)
-        listOClass1Childern.append(child7)
-        listOClass2Childern.append(child8)
-        listOClass1Childern.append(child9)
-        listOClass2Childern.append(child10)
-        listOClass1Childern.append(child11)
-        listOClass2Childern.append(child12)
-        listOClass1Childern.append(child13)
-        listOClass2Childern.append(child14)
-        listOClass1Childern.append(child15)
-        listOClass2Childern.append(child16)
-        listOClass1Childern.append(child17)
-        listOClass2Childern.append(child18)
-
-        evenOddCounter = 1;
-        noisePercentFactor = .10
-        for i in range(len(listOChildren)):
-            if evenOddCounter == 1:
-                gen_two.whiteNoise(listOChildren[i], amountOfDP, noisePercentFactor)
-                evenOddCounter += 1
-            else:
-                gen_two.whiteNoise(listOChildren[i], amountOfDP, noisePercentFactor)
-                evenOddCounter -= 1
-                noisePercentFactor += .10
-
+        data = {}
         #Here adds the lists to the dictionary so they can be returned and added to the database
-        the_return_dict["class1_parent"] = listOParents[0]
-        the_return_dict["class2_parent"] = listOParents[1]
-        the_return_dict["class1_children"] = listOChildren[0]
-        the_return_dict["class2_children"] = listOChildren[1]
+        data["class1_parent"] = parents_class1
+        data["class2_parent"] = parents_class2
+        data["class1_children"] = children_class1
+        data["class2_children"] = children_class2
 
-        return  the_return_dict
-
+        return  data
